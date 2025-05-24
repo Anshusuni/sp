ae490f0d516a580721a1f9a4d9544d3efb0b1617
AbdulRiyan7892 <abdulriyan062@gmail.com>
2025-05-24 23:26:22 +0530

    Update App.js

--- image-analyzer/src/App.js
+++ image-analyzer/src/App.js
@@ -1,10 +1,7 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import UploadPage from './UploadPage';
-import ResultPage from './ResultPage';
-<Routes>
-  <Route path="/" element={<UploadPage />} />
-  <Route path="/result" element={<ResultPage />} />
-</Routes>
+import UploadPage from 'image-analyzer/src/UploadPage.js';
+import ResultPage from 'image-analyzer/src/ResultPage.js';
 
 function App() {
   return (
