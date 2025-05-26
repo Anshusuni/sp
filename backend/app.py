c96321404d394e0240f2b776c85c6c2e0f7b7e18
AbdulRiyan7892 <abdulriyan062@gmail.com>
2025-05-26 06:13:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymongo import MongoClient
+from pymongo import MongoClient 
 from bson import Binary 
 import os
 from dotenv import load_dotenv
